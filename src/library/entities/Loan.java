90812764f124691f629f408306ccc94ccaf68e9d
TrioCoders03 <87842269+TrioCoders03@users.noreply.github.com>
2021-08-25 15:37:01 +1000

    updated

--- src/library/entities/Loan.java
+++ src/library/entities/Loan.java
@@ -12,7 +12,7 @@ public class Loan implements Serializable {
 	private Book book;                    //BoOk
 	private Member member;                //MeMbEr;
 	private Date date;                    //DaTe
-	private loanState state;              //lOaN_sTaTe StAtE
+	private LoanState state;              //lOaN_sTaTe StAtE
 
 	
 	public Loan(int loanId, Book booK, Member member, Date dueDate) {       //change Book bOoK, Member mEmBeR, Date DuE_dAtE
@@ -26,14 +26,14 @@ public class Loan implements Serializable {
 	
 	public void checkOverDue() {                                         // void cHeCk_OvEr_DuE
 		if (state == loanState.CURRENT &&                            //if (StAtE == lOaN_sTaTe.CURRENT
-			Calendar.getInstance().getDate().after(Date))       //Calendar.gEtInStAnCe().gEt_DaTe().after(DaTe)) 
-			this.state = loanState.OVERDUE;	            //this.StAtE = lOaN_sTaTe.OVER_DUE;		
+			Calendar.getInstance().getDate().after(date))       //Calendar.gEtInStAnCe().gEt_DaTe().after(DaTe)) 
+			this.state = loanState.OVER_DUE;	            //this.StAtE = lOaN_sTaTe.OVER_DUE;		
 		
 	}
 
 	
 	public boolean isOverDue() {                                   //boolean Is_OvEr_DuE
-		return state == loanState.OVERDUE;                   //return StAtE == lOaN_sTaTe.OVER_DUE;
+		return state == loanState.OVER_DUE;                   //return StAtE == lOaN_sTaTe.OVER_DUE;
 	}
 
 	
@@ -48,7 +48,7 @@ public class Loan implements Serializable {
 	
 	
 	public String toString() {
-		simpleDateFormat sdf = new simpleDateFormat("dd/MM/yyyy");               //SimpleDateFormat
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");               
 
 		StringBuilder sb = new StringBuilder();
 		sb.append("Loan:  ").append(loanId).append("\n")                        //LoAn_Id
@@ -56,13 +56,13 @@ public class Loan implements Serializable {
 		  .append(member.getlastName()).append(", ").append(member.getFirstName()).append("\n")   //MeMbEr.GeT_LaSt_NaMe   //append(MeMbEr.GeT_FiRsT_NaMe
 		  .append("  Book ").append(book.getId()).append(" : " )                                  //append(BoOk.gEtId())
 		  .append(book.getTitle()).append("\n")                                                 //append(BoOk.gEtTiTlE())
-		  .append("  DueDate: ").append(sdf.format(Date)).append("\n")                         //format(DaTe))
+		  .append("  DueDate: ").append(sdf.format(date)).append("\n")                         //format(DaTe))
 		  .append("  State: ").append(state);	                                                //StAtE	
 		return sb.toString();
 	}
 
 
-	public Member GetMember() {                   //GeT_MeMbEr
+	public Member getMember() {                   //GeT_MeMbEr
 		return member;                        // MeMbEr
 	}
 
